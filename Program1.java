831dd4a203f5f18490370f5289e893ba913ff5fe
Shreeraj-Kadam <kadamshreeraj1999@gmail.com>
2023-08-26 14:28:43 +0530

    Changes made in Program 1

--- Program1.java
+++ Program1.java
@@ -1,6 +1,9 @@
 class Program1{
 public static void main(String[] args){
 System.out.println("Hello World , I am here");
+for(int i = 0 ; i < 10 ; i++){
+System.out.println("Hello World , I am here");
+}
 
 }
 
