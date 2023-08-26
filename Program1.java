ae2c10fc00549d4520ac246858698fb060f1a95b
Shreeraj-Kadam <143249284+Shreeraj-Kadam@users.noreply.github.com>
2023-08-26 14:56:11 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -2,7 +2,7 @@ class Program1{
 public static void main(String[] args){
 System.out.println("Hello World , I am here");
 for(int i = 0 ; i < 10 ; i++){
-System.out.println("Hello World , Pull it man");
+System.out.println("Hello World , Pull it man Pull it manPull it manPull it manPull it man");
 }
 
 }
